7218f6c66f5bf7a74d69fe78df848333828fcc8c
Shuai YUAN <shuai.yuan@dorabot.com>
2019-04-28 11:22:19 +0800

    Use smart ptr for CUDA-related memory

--- src/cuda_utils.h
+++ src/cuda_utils.h
@@ -3,6 +3,7 @@
 
 #include <cuda_runtime.h>
 #include <cstdio>
+#include <memory>
 
 #define CUDA_CHECK(call) { \
   cudaError_t err; \
@@ -12,5 +13,25 @@
     exit(1); \
   } \
 }
+
+template <typename T>
+struct device_ptr_deleter {
+  void operator()(T* ptr) {
+    CUDA_CHECK(cudaFree(ptr));
+  }
+};
+
+template <typename T>
+struct host_ptr_deleter {
+  void operator()(T* ptr) {
+    CUDA_CHECK(cudaFreeHost(ptr));
+  }
+};
+
+template <typename T>
+using unique_ptr_device = std::unique_ptr<T[], device_ptr_deleter<T>>;
+
+template <typename T>
+using unique_ptr_host = std::unique_ptr<T[], host_ptr_deleter<T>>;
 
 #endif /* CUDA_UTILS_H */
